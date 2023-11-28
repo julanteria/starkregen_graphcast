d6998fca25c51f292e8a42b8f951749edbdb9d64
Julian Vorpahl <j.vorpahl@live.de>
2023-11-28 15:13:09 +0100

    daatset

--- intall_dependencies.bat
+++ intall_dependencies.bat
@@ -7,19 +7,19 @@ git clone https://github.com/google-deepmind/graphcast.git
 :: For Windows, assuming Anaconda is installed and added to PATH
 
 :: Create and activate a new conda environment
-call conda create -n graphcastwin python=3.11 -y
-call conda activate graphcastwin
+:: call conda create -n graphcastwin python=3.11 -y
+:: call conda activate graphcastwin
 
 :: Install Python requirements
-python graphcast/setup.py install
-call conda install ipykernel -y
+::python graphcast/setup.py install
+::call conda install ipykernel -y
 
 :: GPU
 :: Uncomment the next line if GPU support is needed and CUDA is installed
 :: pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 :: Install dm-tree (No OS-specific steps needed for Windows)
-pip install dm-tree
+:: pip install dm-tree
 
 :: Create data directories
 mkdir data\params
@@ -27,14 +27,19 @@ mkdir data\stats
 mkdir data\datasets
 
 :: Install gsutil
-pip install gsutil
+::pip install gsutil
 
 :: Download data using gsutil
 gsutil -m cp "gs://dm_graphcast/params/GraphCast - ERA5 1979-2017 - resolution 0.25 - pressure levels 37 - mesh 2to6 - precipitation input and output.npz" .
+
+gsutil -m cp "gs://dm_graphcast/params/GraphCast_operational - ERA5-HRES 1979-2021 - resolution 0.25 - pressure levels 13 - mesh 2to6 - precipitation output only.npz" .
+gsutil -m cp "gs://dm_graphcast/params/GraphCast_small - ERA5 1979-2015 - resolution 1.0 - pressure levels 13 - mesh 2to5 - precipitation input and output.npz" .
+
 gsutil -m cp "gs://dm_graphcast/dataset/source-era5_date-2022-01-01_res-0.25_levels-37_steps-01.nc" data\datasets\
 gsutil -m cp "gs://dm_graphcast/stats/diffs_stddev_by_level.nc" "gs://dm_graphcast/stats/mean_by_level.nc" "gs://dm_graphcast/stats/stddev_by_level.nc" data\stats\
 
 :: Move downloaded parameter file to the appropriate directory
 move "GraphCast - ERA5 1979-2017 - resolution 0.25 - pressure levels 37 - mesh 2to6 - precipitation input and output.npz" "data\params\GraphCast_ERA5_1979-2017_Resolution-0.25_PressureLevels-37_Mesh-2to6_PrecipitationInputOutput.npz"
-
+move "GraphCast_operational - ERA5-HRES 1979-2021 - resolution 0.25 - pressure levels 13 - mesh 2to6 - precipitation output only.npz" "data\params\GraphCast_operational_ERA5-HRES_1979-2021_Resolution-0.25_PressureLevels-13_Mesh-2to6_PrecipitationOutputOnly.npz"
+move "GraphCast_small - ERA5 1979-2015 - resolution 1.0 - pressure levels 13 - mesh 2to5 - precipitation input and output.npz" "data\params\GraphCast_small_ERA5_1979-2015_Resolution-1.0_PressureLevels-13_Mesh-2to5_PrecipitationInputOutput.npz"
 :: End of script
